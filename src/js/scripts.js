13791c6b4a3df68306409cb547ef70a26f980d2f
Balázs Anett <71498781+Balanett@users.noreply.github.com>
2021-02-06 11:21:31 +0100

    You App
    create and remove items

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,2 +1,19 @@
-//can be deleted
-console.log("Console log works!");
+// Create To Do Item / Delete To Do Item
+
+const create = document.getElementById("createBtn")
+const list = document.getElementById("todo-list")
+
+create.addEventListener("click", function () {
+    const todoTxt = window.prompt("What is your todo:")
+    const liItem = document.createElement("li")
+    const btnItem = document.createElement("button")
+    btnItem.addEventListener("click", function () {
+        liItem.remove()
+    })
+    btnItem.innerHTML = "Remove"
+    btnItem.className ="removeBtn"
+    liItem.textContent = todoTxt
+    liItem.appendChild(btnItem)
+    list.append(liItem)
+});
+

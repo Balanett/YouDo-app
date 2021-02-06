6ca018da8e2e73cf11ec3f07fe1c30dde9d4ba1f
Balázs Anett <71498781+Balanett@users.noreply.github.com>
2021-02-06 12:53:18 +0100

    You Do (MZ01-MZ05)

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,10 +1,22 @@
-// Create To Do Item / Delete To Do Item
-
 const create = document.getElementById("createBtn")
 const list = document.getElementById("todo-list")
 
+const inputArea = document.getElementById("input-area")
+let input = ""
+inputArea.addEventListener("keyup", function () {
+    input = inputArea.value
+    if (input.length < 6) {
+        inputArea.style.cssText = "border-color: red"
+        create.disabled = true
+        console.log("igaz")
+    } else {
+        create.disabled = false
+        inputArea.style.cssText = "border-color: green"
+        console.log("hamis")
+    }
+})
+
 create.addEventListener("click", function () {
-    const todoTxt = window.prompt("What is your todo:")
     const liItem = document.createElement("li")
     const btnItem = document.createElement("button")
     btnItem.addEventListener("click", function () {
@@ -12,8 +24,8 @@ create.addEventListener("click", function () {
     })
     btnItem.innerHTML = "Remove"
     btnItem.className ="removeBtn"
-    liItem.textContent = todoTxt
+    liItem.textContent = input
     liItem.appendChild(btnItem)
     list.append(liItem)
+    inputArea.value = ""
 });
-
